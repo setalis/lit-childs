e69d2092f414c74a0a638ea031b22659f5b60d41
setalis <slavrtm@gmail.com>
2026-05-06 17:23:21 +0300

    убрал регистрацию из фронта

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -44,9 +44,10 @@
     </form>
 
     @if (Route::has('register'))
+    <!-- 
         <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
             {{ __('Don\'t have an account?') }}
             <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
         </div>
-    @endif
-</div>
+    --> 
+            @endif
